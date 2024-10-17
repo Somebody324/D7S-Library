1f986eace34facc8eeab2f01bcea94d0c857e428
McAllen Fradejas <58442636+Somebody324@users.noreply.github.com>
2024-10-18 02:35:01 +0800

    Update D7S.cpp

--- src/D7S.cpp
+++ src/D7S.cpp
@@ -1,377 +1,175 @@
-#include "D7S.h"
+#ifndef D7S_H
+#define D7S_H
 
-//constructor/destroyer
-D7SClass::D7SClass() {
-   for (int i = 0; i < 4; i++) {
-      _handlers[i] = NULL;
-   }
+#include <Arduino.h>
+#include <Wire.h>
 
-   _events = 0;
-}
+// If the board is Fishino32 then we need to fix I2C interrupts priority
+#if defined(_FISHINO_PIC32_) || defined(_FISHINO32_) || defined(_FISHINO32_120_) || defined(_FISHINO32_MX470F512H_) || defined(_FISHINO32_MX470F512H_120_)
+   // Include fix file
+   #include "../utils/Fishino32.h"
+   // Define the new Wire instance to use
+   #define WireD7S _Wire
+#else 
+   #define WireD7S Wire
+#endif
 
-//Begin
-//initialize Wire
-void D7SClass::begin() {
-   WireD7S.begin();
-}
+//--- ADDRESS ---
+#define D7S_ADDRESS 0x55 //D7S address on the I2C bus
 
-//Status
-//return the state
-d7s_status D7SClass::getState() {
-   return (d7s_status) (read8bit(0x10, 0x00) & 0x07);
-}
-
-d7s_axis_state D7SClass::getAxisInUse() {
-   return (d7s_axis_state) (read8bit(0x10, 0x01) & 0x03);
-}
-
-//Settings
-//threshold of the sensor (0x00 = standard | 0x01 = sensitive)
-void D7SClass::setThreshold(d7s_threshold threshold) {
-   //check threshold validity
-   if (threshold < 0 || threshold > 1) {
-      return;
-   }
-   uint8_t reg = read8bit(0x10, 0x04);
-   //new register value with the threshold
-   reg = (((reg >> 4) << 1) | (threshold & 0x01)) << 3;
-   //update register
-   write8bit(0x10, 0x04, reg);
-}
-
-//change the axis selection mode
-void D7SClass::setAxis(d7s_axis_settings axisMode) {
-   //check if axisMode is valid
-   if (axisMode < 0 or axisMode > 4) {
-      return;
-   }
-   //read the CTRL register at 0x1004
-   uint8_t reg = read8bit(0x10, 0x04);
-   //new register value with the threshold
-   reg = (axisMode << 4) | (reg & 0x0F);
-   //update register
-   write8bit(0x10, 0x04, reg);
-}
-
-//instantaneus data
-//get instantaneus PGV (during an earthquake) (m/s)
-float D7SClass::getInstantaneusPGV() {
-   return ((float) read16bit(0x20, 0x00)) / 1000;
-}
-
-//get instantaneus PGA (during an earthquake) (m/s^2)
-float D7SClass::getInstantaneusPGA() {
-   return ((float) read16bit(0x20, 0x02)) / 1000;
-}
-
-//identify the intensity 
-uint8_t D7SClass::getIntensity() {
-    float pga = getInstantaneusPGA();
-
-    //define thresholds for the PHIVOLCS intensity scale
-    if (pga == 0) {return 0;}                           //Intensity 0
-    if (pga > 0 && pga < 0.01) {return 1;}              //Intensity I
-    else if (pga >= 0.01 && pga < 0.02) {return 2;}     //Intensity II
-    else if (pga >= 0.02 && pga < 0.05) {return 3;}     //Intensity III
-    else if (pga >= 0.05 && pga < 0.10) {return 4;}     //Intensity IV
-    else if (pga >= 0.10 && pga < 0.25) {return 5;}     //Intensity V
-    else if (pga >= 0.25 && pga < 0.50) {return 6;}     //Intensity VI
-    else if (pga >= 0.50 && pga < 1.00) {return 7;}     //Intensity VII
-    else if (pga >= 1.00 && pga < 2.50) {return 8;}     //Intensity VIII
-    else if (pga >= 2.50 && pga < 5.00) {return 9;}     //Intensity IX
-    else{
-        return 10;}                                     //Intensity X
-}
-
-//clear memory
-//delete initializzazion data
-void D7SClass::clearInstallationData() {
-   write8bit(0x10, 0x05, 0x08);
-}
-
-//delete offset data
-void D7SClass::clearLastestOffsetData() {
-   write8bit(0x10, 0x05, 0x04);
-}
-
-//delete selftest data
-void D7SClass::clearSelftestData() {
-   write8bit(0x10, 0x05, 0x02);
-}
-
-//delete all data
-void D7SClass::clearAllData() {
-   write8bit(0x10, 0x05, 0x0F);
-}
-
-//initialization
-//initialize the d7s
-void D7SClass::initialize() {
-   write8bit(0x10, 0x03, 0x02);
-}
-
-//selftest
-//(OK/ERROR)
-void D7SClass::selftest() {
-   write8bit(0x10, 0x03, 0x04);
-}
-
-//self-diagnostic test (OK/ERROR)
-d7s_mode_status D7SClass::getSelftestResult() {
-   return (d7s_mode_status) ((read8bit(0x10, 0x02) & 0x07) >> 2);
-}
-
-//offset aquisition
-//start offset acquisition and return the rersult (OK/ERROR)
-void D7SClass::acquireOffset() {
-   write8bit(0x10, 0x03, 0x03);
-}
-
-//return the result of offset acquisition test (OK/ERROR)
-d7s_mode_status D7SClass::getAcquireOffsetResult() {
-   return (d7s_mode_status) ((read8bit(0x10, 0x02) & 0x0F) >> 3);
-}
-
-//shutoff event
-uint8_t D7SClass::isInCollapse() {
-   readEvents();
-   return (_events & 0x02) >> 1;
-}
-
-//return true if the shutoff condition is met
-uint8_t D7SClass::isInShutoff() {
-   readEvents();
-   return _events & 0x01;
-}
-
-//reset shutoff/collapse events
-void D7SClass::resetEvents() {
-   read8bit(0x10, 0x02);
-   _events = 0;
-}
-
-//earthquake detection
-//return true if an earthquake is occuring
-uint8_t D7SClass::isEarthquakeOccuring() {
-   return getState() == NORMAL_MODE_NOT_IN_STANBY;
-}
-
-//sensor ready
-uint8_t D7SClass::isReady() {
-   return getState() == NORMAL_MODE;
-}
-
-//interrupt
-//enable interrupt INT1 on specified pin
-void D7SClass::enableInterruptINT1(uint8_t pin) {
-   pinMode(pin, INPUT_PULLUP);
-   attachInterrupt(digitalPinToInterrupt(pin), isr1, FALLING);
-}
-
-//enable interrupt INT2 on specified pin
-void D7SClass::enableInterruptINT2(uint8_t pin) {
-   pinMode(pin, INPUT_PULLUP);
-   attachInterrupt(digitalPinToInterrupt(pin), isr2, CHANGE);
-}
-
-//start interrupt handling
-void D7SClass::startInterruptHandling() {
-   _interruptEnabled = 1;
-}
-
-//stop interrupt handling
-void D7SClass::stopInterruptHandling() {
-   _interruptEnabled = 0;
-}
-
-//assing the handler to the specific event
-void D7SClass::registerInterruptEventHandler(d7s_interrupt_event event, void (*handler) ()) {
-   if (event < 0 || event > 3) {
-      return;
-   }
-   _handlers[event] = handler;
-}
-
-void D7SClass::registerInterruptEventHandler(d7s_interrupt_event event, void (*handler) (float, float, float)) {
-  registerInterruptEventHandler(event, (void (*)()) handler);
-}
+//--- DEBUG ----
+//comment this line to disable all debug information
+//#define DEBUG
 
 
-//do not touch
-//read 8 bit from the specified register
-uint8_t D7SClass::read8bit(uint8_t regH, uint8_t regL) {
+//d7s state
+typedef enum d7s_status {
+   NORMAL_MODE = 0x00,
+   NORMAL_MODE_NOT_IN_STANBY = 0x01, //earthquake in progress
+   INITIAL_INSTALLATION_MODE = 0x02,
+   OFFSET_ACQUISITION_MODE = 0x03,
+   SELFTEST_MODE = 0x04
+};
 
-   //debug
-   #ifdef DEBUG
-      Serial.println("--- read8bit ---");
-      Serial.print("REG: 0x");
-      Serial.print(regH, HEX);
-      Serial.println(regL, HEX);
-   #endif
+//d7s axis settings
+typedef enum d7s_axis_settings {
+   FORCE_YZ = 0x00,
+   FORCE_XZ = 0x01,
+   FORXE_XY = 0x02,
+   AUTO_SWITCH = 0x03,
+   SWITCH_AT_INSTALLATION = 0x04 
+};
 
-   //setting up i2c connection (if you wont use the default i2c pins)
-   WireD7S.beginTransmission(D7S_ADDRESS);
-   
-   //write register address
-   WireD7S.write(regH); 
-   delay(10); 
-   WireD7S.write(regL); 
-   delay(10); 
-   
-   uint8_t status = WireD7S.endTransmission(false);
+//axis state
+typedef enum d7s_axis_state {
+   AXIS_YZ = 0x00,
+   AXIS_XZ = 0x01,
+   AXIS_XY = 0x02
+};
 
-   //debug
-   #ifdef DEBUG
-      Serial.print("[RE-START]: ");
-      Serial.println(status);
-   #endif
+//d7s threshold settings
+typedef enum d7s_threshold {
+   THRESHOLD_HIGH = 0x00,
+   THRESHOLD_LOW = 0x01
+};
 
-   //if the status != 0 there is an error
-   if (status != 0) {
-      //retry
-      return read8bit(regH, regL);
-   }
+//message status (selftes, offset acquisition)
+typedef enum d7s_mode_status {
+   D7S_OK = 0,
+   D7S_ERROR = 1
+};
 
-   //request 1 byte
-   WireD7S.requestFrom(D7S_ADDRESS, 1);
-   //wait until the data is received
-   while (WireD7S.available() < 1)
-      ;
-   //read the data
-   uint8_t data = WireD7S.read();
+//events handled externaly by the using using an handler (the d7s int1, int2 must be connected to interrupt pin)
+typedef enum d7s_interrupt_event {
+   START_EARTHQUAKE = 0, //INT 2
+   END_EARTHQUAKE = 1, //INT 2
+   SHUTOFF_EVENT = 2, //INT 1
+   COLLAPSE_EVENT = 3 //INT 1
+};
 
-   #ifdef DEBUG
-      Serial.println("--- read8bit ---");
-   #endif
-  
-   return data;
-}
 
-//read 16 bit from the specified register
-uint16_t D7SClass::read16bit(uint8_t regH, uint8_t regL) {
+//class D7S
+class D7SClass {
 
-   //DEBUG
-   #ifdef DEBUG
-      Serial.println("--- read16bit ---");
-      Serial.print("REG: 0x");
-      Serial.print(regH, HEX);
-      Serial.println(regL, HEX);
-   #endif
+   public: 
 
-   //setting up i2c connection
-   WireD7S.beginTransmission(D7S_ADDRESS);
-   
-   //write register address
-   WireD7S.write(regH); //register address high
-   delay(10); //delay to prevent freezing
-   WireD7S.write(regL); //register address low
-   delay(10); //delay to prevent freezing
-   
-   //send RE-START message
-   uint8_t status = WireD7S.endTransmission(false);
+      //--- CONSTRUCTOR/DESTROYER ---
+      D7SClass(); //constructor
 
-   //DEBUG
-   #ifdef DEBUG
-      Serial.print("[RE-START]: ");
-      //send RE-START message
-      Serial.println(status);
-   #endif
+      //--- BEGIN ---
+      void begin(); //used to initialize Wire
 
-   //if the status != 0 there is an error
-   if (status != 0) {
-      return read16bit(regH, regL);
-   }
+      //--- STATUS ---
+      d7s_status getState(); //return the currect state
+      d7s_axis_state getAxisInUse(); //return the current axis in use
 
-   //request 2 byte
-   WireD7S.requestFrom(D7S_ADDRESS, 2);
-   //wait until the data is received
-   while (WireD7S.available() < 2)
-      ;
-   //read the data
-   uint8_t msb = WireD7S.read();
-   uint8_t lsb = WireD7S.read();
+      //--- SETTINGS ---
+      void setThreshold(d7s_threshold threshold); //change the threshold in use
+      void setAxis(d7s_axis_settings axisMode); //change the axis selection mode
 
-   #ifdef DEBUG
-      Serial.println("--- read16bit ---");
-   #endif
+      //--- LASTEST DATA ---
+      float getLastestPGV(uint8_t index); //get the lastest PGV at specified index (up to 5) [m/s]
+      float getLastestPGA(uint8_t index); //get the lastest PGA at specified index (up to 5) [m/s^2]
 
-   return (msb << 8) | lsb;
-}
+      //--- INSTANTANEUS DATA ---
+      float getInstantaneusPGV(); //get instantaneus PGV (during an earthquake) [m/s]
+      float getInstantaneusPGA(); //get instantaneus PGA (during an earthquake) [m/s^2]
+      uint8_t getIntensity(); //get the intensity of the earthquake
 
-//write 8 bit to the register specified
-void D7SClass::write8bit(uint8_t regH, uint8_t regL, uint8_t val) {
-   //DEBUG
-   #ifdef DEBUG
-      Serial.println("--- write8bit ---");
-   #endif
+      //--- CLEAR MEMORY ---
+      void clearEarthquakeData(); //delete both the lastest data and the ranked data
+      void clearInstallationData(); //delete initializzazion data
+      void clearLastestOffsetData(); //delete offset data
+      void clearSelftestData(); //delete selftest data
+      void clearAllData(); //delete all data
 
-   //i2c connection
-   WireD7S.beginTransmission(D7S_ADDRESS);
-   
-   //write register address
-   WireD7S.write(regH); //register address high
-   delay(10); //delay to prevent freezing
-   WireD7S.write(regL); //register address low
-   delay(10); //delay to prevent freezing
-   
-   //write data
-   WireD7S.write(val);
-   delay(10);
-   uint8_t status = WireD7S.endTransmission(true);
+      //--- INITIALIZATION ---
+      void initialize(); //initialize the d7s (start the initial installation mode)
 
-   //DEBUG
-   #ifdef DEBUG
-      Serial.print("[STOP]: ");
-      Serial.println(status);
-      Serial.println("--- write8bit ---");
-   #endif
-}
+      //--- SELFTEST ---
+      void selftest(); //trigger self-diagnostic test
+      d7s_mode_status getSelftestResult(); //return the result of self-diagnostic test (OK/ERROR)
 
-//reading events
-void D7SClass::readEvents() {
-   uint8_t events = read8bit(0x10, 0x02) & 0x03;
-   _events |= events;
-}
+      //--- OFFSET ACQUISITION ---
+      void acquireOffset(); //trigger offset acquisition
+      d7s_mode_status getAcquireOffsetResult(); //return the result of offset acquisition test (OK/ERROR)
 
-//interrupt
-void D7SClass::int1() {  //int1 events
-   interrupts();
-   if (_interruptEnabled) {
-      if (isInShutoff()) {
-         if (_handlers[2]) {
-            _handlers[2](); //shutoff
-         }
-      } else {
-         if (_handlers[3]) {
-            _handlers[3](); //collapse
-         }
-      }
-   }
-}
+      //--- SHUTOFF/COLLAPSE EVENT ---
+      //after each earthquakes it's important to reset the events calling resetEvents() to prevent polluting the new data with the old one
+      uint8_t isInCollapse(); //return true if the collapse condition is met (it's the sencond bit of _events)
+      uint8_t isInShutoff(); //return true if the shutoff condition is met (it's the first bit of _events)
+      void resetEvents(); //reset shutoff/collapse events
 
-void D7SClass::int2() {  //int2 events
-   interrupts();
-   if (_interruptEnabled) {
-      if (isEarthquakeOccuring()) {
-         if (_handlers[0]) {
-            _handlers[0](); //START_EARTHQUAKE EVENT
-         }
-      } else { //earthquake ended
-         if (_handlers[1]) {
-            ((void (*)(float, float, float)) _handlers[1])(getLastestSI(0), getLastestPGA(0), getLastestTemperature(0)); //END_EARTHQUAKE EVENT
-         }
-      }
-   }
-}
+      //--- EARTHQUAKE EVENT ---
+      uint8_t isEarthquakeOccuring(); //return true if an earthquake is occuring
 
-//it handle the FALLING event that occur to the INT1 D7S pin 
-void D7SClass::isr1() {
-   D7S.int1();
-}
+      //--- READY STATE ---
+      uint8_t isReady();
 
-//it handle the CHANGE event thant occur to the INT2 D7S pin 
-void D7SClass::isr2() {
-   D7S.int2();
-}
+      //--- INTERRUPT ---
+      void enableInterruptINT1(uint8_t pin); //enable interrupt INT1 on specified pin
+      void enableInterruptINT2(uint8_t pin); //enable interrupt INT2 on specified pin
+      void startInterruptHandling(); //start interrupt handling
+      void stopInterruptHandling(); //stop interrupt handling
+      void registerInterruptEventHandler(d7s_interrupt_event event, void (*handler) ()); //assing the handler to the specific event
+      void registerInterruptEventHandler(d7s_interrupt_event event, void (*handler) (float, float, float)); //assing the handler to the specific event
 
-D7SClass D7S;
+   private:
+      //handler array (it cointaint the pointer to the user defined array)
+      void (*_handlers[4]) ();
+
+      //variable to track event (first bit => SHUTOFF, second bit => COLLAPSE)
+      uint8_t _events;
+
+      //enable interrupt handling
+      uint8_t _interruptEnabled;
+
+      //--- READ ---
+      uint8_t read8bit(uint8_t regH, uint8_t regL); //read 8 bit from the specified register
+      uint16_t read16bit(uint8_t regH, uint8_t regL); //read 16 bit from the specified register
+
+      //--- WRITE ---
+      void write8bit(uint8_t regH, uint8_t regL, uint8_t val); //write 8 bit to the register specified
+
+      //--- READ EVENTS ---
+      void readEvents(); //read the event (SHUTOFF/COLLAPSE) from the EVENT register
+
+      //--- EVENT HANDLER ---
+      void int1(); //handle the INT1 events
+      void int2(); //handle the INT2 events
+
+      //--- ISR HANDLER ---
+      static void isr1(); //it handle the FALLING event that occur to the INT1 D7S pin (glue routine)
+      static void isr2(); //it handle the CHANGE event thant occur to the INT2 D7S pin (glue routine)
+
+      // Fishino32 cannot handle CHANGE mode on interrupts, so we need to register FALLING mode first and on the isr register
+      // as RISING the same pin detaching the previus interrupt
+      #if defined(_FISHINO_PIC32_) || defined(_FISHINO32_) || defined(_FISHINO32_120_) || defined(_FISHINO32_MX470F512H_) || defined(_FISHINO32_MX470F512H_120_)
+         uint8_t pinINT2;
+      #endif
+
+};
+
+extern D7SClass D7S;
+
+#endif
